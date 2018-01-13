8cafcb3c568982e7921f050e976f77c6d0a60dfe
Bhathiya Perera <bhathiya91@live.com>
2018-01-13 04:50:58 -0800

    Added Draft.js, enzyme

---
+++ src/components/editor/Editor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Editor, EditorState } from 'draft-js';
+import './Editor.css';
+
+class Editor extends Component {
+  render() {
+    return <textarea />;
+  }
+}
+
+export default Editor;
